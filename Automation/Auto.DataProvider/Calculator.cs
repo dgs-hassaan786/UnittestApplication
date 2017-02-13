70cb5f917bb070fd1ae599f6ced3a95c28ce55dd
hassaan.khan@dgsworld.com <hassaan09>
2017-02-13 21:48:20 +0500

    solution added

---
+++ Automation/Auto.DataProvider/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto.DataProvider
+{
+    public class Calculator
+    {
+        public T Add<T>(T a, T b)
+        {
+            return (T)((dynamic)a + (dynamic)b);
+        }
+
+        public T Divide<T>(T a, T b)
+        {
+            return (T)((dynamic)a / (dynamic)b);
+        }
+
+        public T Multiply<T>(T a, T b)
+        {
+            return (T)((dynamic)a * (dynamic)b);
+        }
+
+        public T Subtract<T>(T a, T b)
+        {
+            return (T)((dynamic)a - (dynamic)b);
+        }
+
+        public T Modulus<T>(T a, T b)
+        {
+            return (T)((dynamic)a % (dynamic)b);
+        }
+    }
+}
---
+++ Automation/Auto.MSUnit/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Auto.DataProvider;
+
+namespace Auto.MSUnit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            int actual = 6;
+            var calculator = new Calculator();
+            int result = calculator.Add(1, 5);
+            Assert.AreEqual(actual, result);
+        }
+
+        [TestMethod]
+        public void TestDivide()
+        {
+            int actual = 4;
+            var calculator = new Calculator();
+            int result = calculator.Divide(20,5);
+            Assert.AreEqual(actual, result);
+        }
+
+        [TestMethod]
+        public void TestFaultyDivide()
+        {
+            int actual = 4;
+            var calculator = new Calculator();
+            int result = calculator.Modulus(20, 5);
+            Assert.AreEqual(actual, result);
+        }
+
+        [TestMethod]
+        public void TestMultiply()
+        {
+            var actual = 2.08;
+            var calculator = new Calculator();
+            var result = calculator.Multiply(1.3, 1.6);
+            Assert.AreEqual(actual, result);
+        }
+
+    }
+}
