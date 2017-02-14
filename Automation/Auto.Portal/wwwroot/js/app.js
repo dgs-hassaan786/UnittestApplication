aed6e7b75f0d254dfd870531c7dce3ad884a196f
hassaan.khan@dgsworld.com <hassaan09>
2017-02-14 14:43:59 +0500

    asp.net core project added with sample angular integration

---
+++ Automation/Auto.Portal/wwwroot/js/app.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('my_app', []);
+
+    app.controller('UserController', function ($scope) {
+
+        debugger
+
+        $scope.sayHello = function () {
+            debugger
+            return 'Hello World';
+        }
+
+    });
+
+})();
