865f9336235b3235acc32f84e91ef465f9475263
GhazanfarKhan <ghazanfar.khan@dgsworld.com>
2017-02-15 17:29:13 +0500

    reference js updated

--- Automation/Auto.Portal/wwwroot/_references.js
+++ Automation/Auto.Portal/wwwroot/_references.js
@@ -3,5 +3,7 @@
 /// <reference path="../karma.conf.js" />
 /// <reference path="js/app.js" />
 /// <reference path="js/site.js" />
+/// <reference path="lib/angular/angular.js" />
+/// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
